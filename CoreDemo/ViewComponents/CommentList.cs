72bbe3ab53e7a135860afaf448efb8b1a4ba4e28
Fatime668 <tu7dkjdue@code.edu.az>
2022-11-08 21:02:24 +0400

    newCommit

---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,32 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CoreDemo.ViewComponents
+{
+    public class CommentList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentValues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    Id = 1,
+                    Username = "Fatima"
+                },
+                new UserComment
+                {
+                    Id=2,
+                    Username = "Aynura"
+                },
+                 new UserComment
+                {
+                    Id=3,
+                    Username = "Gunel"
+                }
+            };
+            return View(commentValues);
+        }
+    }
+}
